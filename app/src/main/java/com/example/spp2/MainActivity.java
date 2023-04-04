0a4351243cefa489fb5d4d1cb5ec79f73a4ededc
wangchaohong <wangchaohong@risound-china.com>
2023-04-04 16:57:49 +0800

    BUG_id[0003] [spp蓝牙串口通信]
    /************************************************************
    
    提交原因：
    1、注册按键和列表点击事件
    
    ************************************************************/

--- app/src/main/java/com/example/spp2/MainActivity.java
+++ app/src/main/java/com/example/spp2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.spp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //绑定按钮
+        Button bt0 = (Button) findViewById(R.id.button0);
+        Button bt1 = (Button) findViewById(R.id.button1);
+        Button bt2 = (Button) findViewById(R.id.button2);
+        Button bt3 = (Button) findViewById(R.id.button3);
+        Button bt4 = (Button) findViewById(R.id.button4);
+        //新建一个监视器类对象
+        MyClickListener mcl = new MyClickListener();
+        //button注册监视器
+        bt0.setOnClickListener(mcl);
+        bt1.setOnClickListener(mcl);
+        bt2.setOnClickListener(mcl);
+        bt3.setOnClickListener(mcl);
+        bt4.setOnClickListener(mcl);
+
+        ListView listView = (ListView) findViewById(R.id.ListView0);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
+    }
+
+    class MyClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
